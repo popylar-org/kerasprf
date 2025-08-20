488e578b0ef69c9197fcbbb8906dc579c3aeb362
maltelueken <malte_lueken@arcor.de>
2025-08-20 13:40:55 +0200

    Add basic adapter and transformations

--- kerasprf/model/backend/base_model.py
+++ kerasprf/model/backend/base_model.py
@@ -1,5 +1,11 @@
 
+import warnings
+
 import keras
+
+from tqdm import tqdm
+
+from kerasprf.adapter import Adapter
 
 
 match keras.backend.backend():
@@ -11,7 +17,83 @@ match keras.backend.backend():
         from .torch_model import TorchModel as BaseBackendModel
     case other:
         raise ValueError(f"Backend '{other}' is not supported.")
-    
+
 
 class BackendModel(BaseBackendModel):
-    pass
+    def __init__(self, adapter=None, params=None):
+        super().__init__()
+
+        if adapter is None:
+            adapter = self.create_default_adapter()
+
+        if params is None:
+            params = self.set_default_params()
+
+        self.check_required_params(params)
+
+        self.adapter = adapter
+
+        params_adapted = self.adapter(params)
+        
+        for key, val in params_adapted.items():
+            setattr(self, key, keras.Variable(val, dtype="float32", name=key))
+
+    @property
+    def required_params(self):
+        return set()
+
+    @property
+    def params(self):
+        trainable_varialbes_dict = {param.name: param.value for param in self.trainable_variables}
+        return self.adapter(trainable_varialbes_dict, inverse=True)
+    
+    @staticmethod
+    def create_default_adapter():
+        return Adapter()
+    
+    @staticmethod
+    def set_default_params():
+        return dict()
+    
+
+    def check_required_params(self, params):
+        required_params = set(self.required_params)
+        user_params = set(params.keys())
+        diff_required = required_params - user_params
+        diff_user = user_params - required_params
+
+        if len(diff_required) > 0:
+            raise ValueError(f"Missing required parameters: {diff_required}")
+        
+        if len(diff_user) > 0:
+            warnings.warn(f"Ignoring unused parameters: {diff_user}")
+
+
+    def fit(self, x, y, num_steps=1000):
+        state = self.get_state()
+
+        with tqdm(range(num_steps)) as pbar:
+            for _ in pbar:
+                logs, state = self.update_model_weights(x, y, state)
+                
+                if logs:
+                    display_logs = {}
+                    for key, value in logs.items():
+                        try:
+                            if hasattr(value, 'numpy'):
+                                display_logs[key] = float(value.numpy())
+                            else:
+                                display_logs[key] = float(value)
+                        except (AttributeError, TypeError):
+                            display_logs[key] = str(value)
+                    
+                    pbar.set_postfix(display_logs)
+
+        if state is not None:
+            trainable_variables, non_trainable_variables, optimizer_variables, metrics_variables = state
+            for variable, value in zip(self.trainable_variables, trainable_variables):
+                variable.assign(value)
+            for variable, value in zip(self.non_trainable_variables, non_trainable_variables):
+                variable.assign(value)
+
+        return logs
--- kerasprf/model/gaussian_2d.py
+++ kerasprf/model/gaussian_2d.py
@@ -1,55 +1,39 @@
 
 import keras
 
-from tqdm import tqdm
-
 from .backend.base_model import BackendModel
+
+from kerasprf.adapter import Adapter
 
 
 class Gaussian2DModel(BackendModel):
-    def __init__(self, centroid, sigma, scale):
-        super().__init__()
-        self.centroid = keras.Variable(centroid, dtype="float32", name="centroid")
-        self.sigma = keras.Variable(sigma, dtype="float32", name="sigma")
-        self.scale = scale
+    @property
+    def required_params(self):
+        return set(["centroid", "sigma"])
+    
+    @staticmethod
+    def create_default_adapter():
+        return (Adapter()
+            .transform(include="sigma", forward_fun=keras.ops.log, inverse_fun=keras.ops.exp)
+            .broadcast(include="centroid", shape=(1, 1, 2))
+        )
+    
+    @staticmethod
+    def set_default_params():
+        return dict(
+            centroid=keras.ops.array([0, 0]),
+            sigma=1.0
+        )
 
 
     def call(self, grid, stimulus, training=None):
-        x = keras.ops.exp(-(keras.ops.sum((grid - self.centroid[None, None, :])**2, axis=-1) / (2 * self.sigma**2))) * stimulus
-        x = keras.ops.sum(x, axis=(0, 1)) / self.scale
+        params = self.params
+
+        x = keras.ops.exp(-(keras.ops.sum((grid - params["centroid"])**2, axis=-1) / (2 * params["sigma"]**2))) * stimulus
+        x = keras.ops.sum(x, axis=(0, 1))
 
         if not training:
             return keras.ops.convert_to_numpy(x)
 
         return x
-    
-
-    def fit(self, x, y, num_steps=1000):
-        state = self.get_state()
-
-        with tqdm(range(num_steps)) as pbar:
-            for _ in pbar:
-                logs, state = self.update_model_weights(x, y, state)
-                
-                if logs:
-                    display_logs = {}
-                    for key, value in logs.items():
-                        try:
-                            if hasattr(value, 'numpy'):
-                                display_logs[key] = float(value.numpy())
-                            else:
-                                display_logs[key] = float(value)
-                        except (AttributeError, TypeError):
-                            display_logs[key] = str(value)
-                    
-                    pbar.set_postfix(display_logs)
-
-        if state is not None:
-            trainable_variables, non_trainable_variables, optimizer_variables, metrics_variables = state
-            for variable, value in zip(self.trainable_variables, trainable_variables):
-                variable.assign(value)
-            for variable, value in zip(self.non_trainable_variables, non_trainable_variables):
-                variable.assign(value)
-
-        return logs
     
